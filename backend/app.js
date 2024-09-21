400c93c2cd77564a639aaea4e6c73d09e0242453
PRAVIT PRASHANT NAIK <108272383+Pravitnk@users.noreply.github.com>
2024-09-21 18:38:05 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path"; 
+
+const _dirname = path.resolve();
 
 export const app = express();
 
@@ -22,11 +25,9 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 
 import { userRouter } from "./routes/User.js";
-import path from "path"; // Import path to handle file paths
 
 app.use("/api/v1", userRouter);
 
-const _dirname = path.resolve();
 app.use(express.static(path.join(_dirname, "Front_end", "dist")));
 app.get("*", (_, res) => {
   res.sendFile(path.join(_dirname, "Front_end", "dist", "index.html"));
